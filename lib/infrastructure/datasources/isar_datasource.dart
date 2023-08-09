246af8e8651647658fdbfd4763af6d89a7bc87fb
jose david <81387297+josedavid1996@users.noreply.github.com>
2023-08-09 10:52:10 -0500

    se aplicaron los datasource y repositorios

---
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -0,0 +1,22 @@
+import 'package:cinemapedia/config/domain/datasources/local_storage_datasource.dart';
+import 'package:cinemapedia/config/domain/entities/movie.dart';
+
+class IsarDatasource extends LocalStorageDatasource {
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    // TODO: implement isMovieFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    // TODO: implement loadMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
+    throw UnimplementedError();
+  }
+}
